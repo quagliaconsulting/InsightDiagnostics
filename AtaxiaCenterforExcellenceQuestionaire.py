524172306cd457dd9c4e8f7c775ed9fecb9b9837
Justin <mascottoj@gmail.com>
2024-04-17 08:50:57 -0400

    add login page

--- AtaxiaCenterforExcellenceQuestionaire.py
+++ AtaxiaCenterforExcellenceQuestionaire.py
@@ -6,21 +6,280 @@ import os
 from pymongo import MongoClient
 import pdb
 from datetime import datetime as dt
-
 # pdb.set_trace()  # Debugger will activate here
 
 write_to_cloud = True
 
-passw = '6BkKNt5tjE'
-usr = 'patientsort'
-connStr = f'mongodb+srv://{usr}:{passw}@patientsortdb.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
-# connStr = f'mongodb://patientsort:i*UF8^4#o*@localhost:27017/?authSource=admin'
+connStr = os.getenv('MONGO_CONNECTION_URL')
 # Define the grouped questions
+
+questions = [
+    {
+        "prompt": "Over the last two weeks how often have you been bothered by the following problems?",
+        "instruction": "0- Not at all, 1 -Several days, 2- More than half the days, 3 - Nearly every day, 4 - Frequently, 5 - Very Frequently",
+        "group": "Psychiatric Symptoms",
+        "type":"slider",
+        "config": {
+            "min":0,
+            "max":5,
+            "value": 2
+        }
+    },
+    {
+        "prompt": "Feeling nervous, anxious, or on edge?",
+        "instruction": "0- Not at all, 1 -Several days, 2- More than half the days, 3 - Nearly every day, 4 - Frequently, 5 - Very Frequently",
+        "group": "Psychiatric Symptoms",
+        "type":"slider",
+        "config": {
+            "min":0,
+            "max":5,
+            "value": 2
+        }
+    },
+    {
+        "prompt": "Little interest or pleasure in doing things?",
+        "instruction": "0- Not at all, 1 -Several days, 2- More than half the days, 3 - Nearly every day, 4 - Frequently, 5 - Very Frequently",
+        "group": "Psychiatric Symptoms",
+        "type":"slider",
+        "config": {
+            "min":0,
+            "max":5,
+            "value": 2
+        }
+    },
+    {
+        "prompt": "Feeling down, depressed or hopeless?",
+        "instruction": "0- Not at all, 1 -Several days, 2- More than half the days, 3 - Nearly every day, 4 - Frequently, 5 - Very Frequently",
+        "group": "Psychiatric Symptoms",
+        "type":"slider",
+        "config": {
+            "min":0,
+            "max":5,
+            "value": 2
+        }
+    },
+    {
+        "prompt": "I have quick changes in my emotions",
+        "instruction": "For each statement below, please rate it 0-5 with 0 being never and 5 being almost always true. ",
+        "group": "Psychiatric Symptoms",
+        "type":"slider",
+        "config": {
+            "min":0,
+            "max":5,
+            "value": 2
+        }
+    },
+    {
+        "prompt": "I have trouble with my thinking or memory that does not include difficulty finding names of people or places.",
+        "instruction": "For each statement below, please rate it 0-5 with 0 being never and 5 being almost always true. ",
+        "group": "Cognition",
+        "type":"slider",
+        "config": {
+            "min":0,
+            "max":5,
+            "value": 2
+        }
+    },
+    {
+        "prompt": "I have double vision (seeing 2 of things or an object and its shadow).",
+        "instruction": "For each statement below, please rate it 0-5 with 0 being never and 5 being almost always true. ",
+        "group": "Vision",
+        "type":"slider",
+        "config": {
+            "min":0,
+            "max":5,
+            "value": 2
+        }
+    },
+    {
+        "prompt": "I see the room/world shake sometimes (like the shaking of a camera on film).",
+        "instruction": "For each statement below, please rate it 0-5 with 0 being never and 5 being almost always true. ",
+        "group": "Vision",
+        "type":"slider",
+        "config": {
+            "min":0,
+            "max":5,
+            "value": 2
+        }
+    },
+    {
+        "prompt": "Other people ask me to repeat myself when I am talking because my speech is slurred or soft.",
+        "instruction": "For each statement below, please rate it 0-5 with 0 being never and 5 being almost always true. ",
+        "group": "Speech/Swallow",
+        "type":"slider",
+        "config": {
+            "min":0,
+            "max":5,
+            "value": 2
+        }
+    },
+    {
+        "prompt": "I have trouble swallowing or frequently choke or cough when eating or drinking.",
+        "instruction": "For each statement below, please rate it 0-5 with 0 being never and 5 being almost always true. ",
+        "group": "Speech/Swallow",
+        "type":"slider",
+        "config": {
+            "min":0,
+            "max":5,
+            "value": 2
+        }
+    },
+    {
+        "prompt": "I get lightheaded or feel like I will pass out if I stand up quickly from laying or sitting or bending over to standing",
+        "instruction": "For each statement below, please rate it 0-5 with 0 being never and 5 being almost always true. ",
+        "group": "Autonomic",
+        "type":"slider",
+        "config": {
+            "min":0,
+            "max":5,
+            "value": 2
+        }
+    },
+    {
+        "prompt": "I have erectile dysfunction. (0 for not applicable)",
+        "instruction": "For each statement below, please rate it 0-5 with 0 being never and 5 being almost always true. ",
+        "group": "Autonomic",
+        "type":"slider",
+        "config": {
+            "min":0,
+            "max":5,
+            "value": 2
+        }
+    },
+    {
+        "prompt": "My hands are clumsy sometimes.",
+        "instruction": "For each statement below, please rate it 0-5 with 0 being never and 5 being almost always true. ",
+        "group": "Fine Motor",
+        "type":"slider",
+        "config": {
+            "min":0,
+            "max":5,
+            "value": 2
+        }
+    },
+    {
+        "prompt": "Typing, texting, or other fine motor tasks are more difficult than they used to be. ",
+        "instruction": "For each statement below, please rate it 0-5 with 0 being never and 5 being almost always true. ",
+        "group": "Fine Motor",
+        "type":"slider",
+        "config": {
+            "min":0,
+            "max":5,
+            "value": 2
+        }
+    },
+    {
+        "prompt": "I have constipation, act out my dreams, or my sense of smell or taste has decreased or gone away.  ",
+        "instruction": "For each statement below, please rate it 0-5 with 0 being never and 5 being almost always true. ",
+        "group": "Additional neurologic signs",
+        "type":"slider",
+        "config": {
+            "min":0,
+            "max":5,
+            "value": 2
+        }
+    },
+    {
+        "prompt": "I have muscle stiffness or muscle cramps",
+        "instruction": "For each statement below, please rate it 0-5 with 0 being never and 5 being almost always true. ",
+        "group": "Additional neurologic signs",
+        "type":"slider",
+        "config": {
+            "min":0,
+            "max":5,
+            "value": 2
+        }
+    },
+    {
+        "prompt": "I have trouble walking.",
+        "instruction": "For each statement below, please rate it 0-5 with 0 being never and 5 being almost always true. ",
+        "group": "Balance",
+        "type":"slider",
+        "config": {
+            "min":0,
+            "max":5,
+            "value": 2
+        }
+    },
+    {
+        "prompt": "When I go down a long hall, I most commonly use this for extra support",
+        "instruction": "0 - Nothing, 1 - Wall Occasionally, 2 - Cane, 3 - Walker, 4 - Wheelchair, 5 - I do not go anywhere without people helping me",
+        "group": "Balance",
+        "type":"slider",
+        "config": {
+            "min":0,
+            "max":5,
+            "value": 2
+        }
+    },
+    {
+        "prompt": "I have fallen this many times over the last 3 months",
+        "instruction": "0 - None, 1 - One to Two, 2 - Three to Six, 3 - Six to Ten, 4 - Ten to Fifteen, 5 - Fifteen+",
+        "group": "Balance",
+        "type":"slider",
+        "config": {
+            "min":0,
+            "max":5,
+            "value": 2
+        }
+    },
+    {
+        "prompt": "Over the last 4 weeks, how difficult have these problems made it for you to do your work, take care of things at home, or interact with other people?",
+        "instruction": " 0 - no difficulty to 5 - extremely difficult",
+        "group": "General Question",
+        "type":"slider",
+        "config": {
+            "min":0,
+            "max":5,
+            "value": 2
+        }
+    },
+    {
+        "prompt": "What is the most difficult symptom?",
+        "instruction": "0 - Gait/walking, 1 - Controlling Hands, 2 - Speech, 3 - Vision, 4 - Dizziness, 5 - Other",
+        "group": "General Question",
+        "type":"slider",
+        "config": {
+            "min":0,
+            "max":5,
+            "value": 2
+        }
+    },
+    {
+        "prompt": "How satisfied are you with your clinical care at the JHU Ataxia Center?",
+        "instruction": "0 - not satisfied 5 - extremely satisfied",
+        "group": "General Question",
+        "type":"slider",
+        "config": {
+            "min":0,
+            "max":5,
+            "value": 2
+        }
+    },
+]
+
+questionnare = {
+    "questions":questions,
+    "name": "Ataxia Questionnaire",
+    "version": 1
+}
+
+
+client = MongoClient(connStr)
+
+# Access database named 'mydatabase'
+db = client['patientsort']
+# Access collection named 'mycollection' in the database
+collection = db['questionnaire']
+
+collection.insert_one(questionnare)
+
+
 grouped_questions = {
     'Psychiatric Symptoms': [
-        'Do you ever find yourself feeling nervous, anxious, or on edge?',
-        'Do you ever find yourself not being able to stop or control worrying?',
-        'Do you have little interest or pleasure in doing things?',
+        'Over the last two weeks how often have you been bothered by the following problems?',
+        'Feeling nervous, anxious, or on edge?',
+        'Not being able to stop or control worrying?',
         'Do you find yourself feeling down, depressed, or hopeless?',
         'I have frequent and quick changes in my emotions.',
         "I have thoughts of suicide."
---
+++ IDPhysicianApp/page/login.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from utils.db_utils import get_mongo_db
+from utils.debug_logging import debug_log
+
+def login_page():
+    st.title("Ataxia Questionaire Physician Portal")
+
+
+    # Display the logos
+    col1, col2 = st.columns(2)
+    col1.image("../logos/JH.png", use_column_width=True, caption="Ataxia Center for Excellence")
+    col2.image("../logos/ID.png", use_column_width=True, caption="Insight Diagnostics")
+    with st.sidebar:
+        st.header("Physician Login")
+        username = st.text_input("Username")
+        password = st.text_input("Password", type='password')
+        if st.button("Login"):
+            query = {
+                'username':username,
+                'password':password
+            }
+            db = get_mongo_db()
+            user_collection = db['users']
+            user = user_collection.find_one(query)
+            debug_log(user)
+            if user:
+                st.session_state["logged_in"] = True
+                st.session_state["user"] = user
+                st.rerun()
